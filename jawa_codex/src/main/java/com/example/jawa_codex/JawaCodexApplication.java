6defb3baa1601d0c2f132a1145505acef21aa9ac
Gerboii <ger.doblas@gmail.com>
2026-04-16 11:13:30 +0200

    Initial commit

---
+++ jawa_codex/src/main/java/com/example/jawa_codex/JawaCodexApplication.java
@@ -0,0 +1,13 @@
+package com.example.jawa_codex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JawaCodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JawaCodexApplication.class, args);
+	}
+
+}
---
+++ jawa_codex/src/test/java/com/example/jawa_codex/JawaCodexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jawa_codex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JawaCodexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
