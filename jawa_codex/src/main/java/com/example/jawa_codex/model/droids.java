7a756c09df1e2b5b42f4dcc6aaa53453382e7076
Gerboii <ger.doblas@gmail.com>
2026-04-16 13:05:59 +0200

    Implementa constructores básicos a los modelos

--- jawa_codex/src/main/java/com/example/jawa_codex/model/droids.java
+++ jawa_codex/src/main/java/com/example/jawa_codex/model/droids.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table
 public class droids {
