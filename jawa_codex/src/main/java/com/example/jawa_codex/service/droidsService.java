b30e2374d8e4126b4d8f0b2a8052e2da6d8193e4
Gerboii <ger.doblas@gmail.com>
2026-04-16 14:09:04 +0200

    Prepara Service

--- jawa_codex/src/main/java/com/example/jawa_codex/service/droidsService.java
+++ jawa_codex/src/main/java/com/example/jawa_codex/service/droidsService.java
@@ -1,4 +1,13 @@
 package com.example.jawa_codex.service;
 
+import com.example.jawa_codex.repository.droidsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class droidsService {
+    @Autowired
+    private droidsRepository droidsRepository;
 }
--- jawa_codex/src/main/java/com/example/jawa_codex/service/manufacturersService.java
+++ jawa_codex/src/main/java/com/example/jawa_codex/service/manufacturersService.java
@@ -1,4 +1,14 @@
 package com.example.jawa_codex.service;
 
+import com.example.jawa_codex.repository.droidsRepository;
+import com.example.jawa_codex.repository.manufacturersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class manufacturersService {
+    @Autowired
+    private manufacturersRepository manufacturersRepository;
 }
