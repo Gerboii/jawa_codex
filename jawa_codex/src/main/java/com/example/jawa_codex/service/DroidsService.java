be5dc521c8e7a4d76b39ce14e4fd608d1050a169
Gerboii <ger.doblas@gmail.com>
2026-04-19 18:46:30 +0200

    Actualiza DroidService

--- jawa_codex/src/main/java/com/example/jawa_codex/service/DroidsService.java
+++ jawa_codex/src/main/java/com/example/jawa_codex/service/DroidsService.java
@@ -6,23 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class DroidsService {
     @Autowired
     private DroidsRepository droidsRepository;
 
-    //Recibe id -> Devuelve Droid si existe
-    public Droids buscarDroid(Long id){
-        if(droidsRepository.findById(id).isPresent()){
-            return droidsRepository.findById(id).get();
-        }else{
-            return null;
-        }
+    //Devuelve lista con todos los Droids
+    public List<Droids> getAllDroids() {
+        return droidsRepository.findAll();
     }
-    public void eliminarDroid(Droids droid){
-        droidsRepository.delete(droid);
+
+    //Recibe ID -> Devuelve Optional Droid
+    public Optional<Droids> getById(long id){
+        return droidsRepository.findById(id);
     }
+
+    //Recibe Id -> Borra y devuelve True si existe, si no existe devuelve False
+    public Boolean eliminarDroid(long id){
+       if (getById(id).isPresent()) {
+           droidsRepository.deleteById(id);
+           return true;
+       }
+       return false;
+    }
+    //Recibe droid -> Guarda
     public void agregarDroid(Droids droid){
         droidsRepository.save(droid);
     }
